f09ee0754825c8b02c544587c0e72aee4bfd8981
zhanggj <850295855@qq.com>
2022-09-23 09:46:45 +0800

    123

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,19 +2,18 @@ module.exports = {
     title: 'Hello VuePress',
     bask:'/emsDocs/',
     description: 'Just playing around',
-    theme: 'vdoing',
+    // theme: 'vdoing',
     // 导航栏
     themeConfig: {
         nav: [
             { text: 'Home', link: '/' },
-            { text: 'Part1', link: '/@pages/archivesPage' },
+            { text: '前端手册', link: '/@pages/archivesPage' },
             { text: 'Part2', link: '/@pages/categoriesPage' },
-            { text: 'External', link: 'https://google.com' },
         ],
         // 侧边栏
         sidebar: [
-            '/@pages/tagsPage',
-            '/@pages/archivesPage',
+            ['/@pages/tagsPage', 'tagsPage'],
+            ['/@pages/archivesPage', '前端手册'],
             ['/@pages/categoriesPage', '随便起个标题，会显示在左边栏']
         ]
     }
